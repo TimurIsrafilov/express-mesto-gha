2e6cad38dc61bd462cde68e2d75db7d4790741e1
Timur Israfilov <timooooon11@yandex.ru>
2023-03-08 09:07:14 +0300

    PW13-second-commit-for-third-rewiev

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: 'Too many request from this IP',
