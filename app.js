18cc49491e6f34bfeccb7d9f3d8259dc58736a25
Timur Israfilov <timooooon11@yandex.ru>
2023-03-27 00:32:55 +0300

    PW14 validation-joy-fixed3

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/(https?:\/\/)?([\w.]+)/).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
   }),
 }), createUser);
 
