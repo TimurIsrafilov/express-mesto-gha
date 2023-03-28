aa692f56780a3526b3408aa188c38ca1804cedc0
Timur Israfilov <timooooon11@yandex.ru>
2023-03-28 03:01:17 +0300

    PW14 fixed card deletion3

--- errors/permittion-error.js
+++ errors/permittion-error.js
@@ -1,8 +1,8 @@
-class ValidatationError extends Error {
+class PermittionError extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = 400;
+    this.statusCode = 403;
   }
 }
 
-module.exports = ValidatationError;
+module.exports = PermittionError;
